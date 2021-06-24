603d6ad85208b6de9530a4938775f1dc8c20bcd9
Nwabuokei Nnamdi <nwabuokeinnamdi@gmail.com>
2021-06-24 09:28:29 +0100

    refactored attempt.js

--- attempt.js
+++ attempt.js
@@ -1,44 +1,22 @@
 module.exports.attempts = (avail, allowed = [], pref) => {
-
-    // get data from avail that is in allowed into result
-    // get data from result that is in pref
-    let firstSeive = []
-    let preffered = []
-
-    for (let data of avail) {
-        if (allowed.includes(data) || allowed.includes('any')) {
-            firstSeive.push(data)
-        }
+  // get data from avail that is in allowed into result
+  // get data from result that is in pref
+  const result = [];
+  let firstSeive = avail.map((data) => ((allowed.includes(data) || allowed.includes('any')) ? data : ''));
+  firstSeive = firstSeive.filter((v) => v !== '');
+  // check for pref
+  if (firstSeive.length > 0) {
+    let preffered = firstSeive.map((data) => (pref.includes(data) || pref.includes('any') ? data : ''));
+    preffered = preffered.filter((v) => v !== '');
+    if (preffered.length > 0) {
+      return preffered;
     }
-
-
-    // check for pref
-    if (firstSeive.length > 0) {
-        for (let data of firstSeive) {
-            if (pref.includes(data) || pref.includes('any') ) {
-                preffered.push(data);
-            }
-        }
-
-       
-        if (preffered.length > 0) {
-            return preffered
-        }
-        if (preffered.length == 0 && Math.max(...firstSeive) < Math.max(...pref)) {
-            preffered.push(Math.min(...firstSeive));
-        } else {
-            preffered.push(Math.max(...firstSeive))
-        }
-
+    if (preffered.length === 0 && Math.max(...firstSeive) < Math.max(...pref)) {
+      preffered.push(Math.min(...firstSeive));
+    } else {
+      preffered.push(Math.max(...firstSeive));
     }
-
-
     return preffered;
-
-
-
-
-
-}
-
-
+  }
+  return result;
+};
