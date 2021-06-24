23ce2e1c4435960877946a5a7deb69fdd5dbcf67
Nwabuokei Nnamdi <nwabuokeinnamdi@gmail.com>
2021-06-24 13:47:51 +0100

    refactored code based on .eslintrc.yml

--- attempt.js
+++ attempt.js
@@ -1,20 +1,26 @@
-module.exports.attempts = (avail, allowed = [], pref) => {
+module.exports.attempts = ( avail, allowed, pref ) =>
+{
   // get data from avail that is in allowed into result
   // get data from result that is in pref
   const result = [];
-  let firstSeive = avail.map((data) => ((allowed.includes(data) || allowed.includes('any')) ? data : ''));
-  firstSeive = firstSeive.filter((v) => v !== '');
+  let firstSeive = avail.map( ( data ) => ( ( allowed.includes( data ) || allowed.includes( 'any' ) ) ? data : '' ) );
+  firstSeive = firstSeive.filter( ( v ) => v !== '' );
   // check for pref
-  if (firstSeive.length > 0) {
-    let preffered = firstSeive.map((data) => (pref.includes(data) || pref.includes('any') ? data : ''));
-    preffered = preffered.filter((v) => v !== '');
-    if (preffered.length > 0) {
+  if( firstSeive.length > 0 )
+  {
+    let preffered = firstSeive.map( ( data ) => ( pref.includes( data ) || pref.includes( 'any' ) ? data : '' ) );
+    preffered = preffered.filter( ( v ) => v !== '' );
+    if( preffered.length > 0 )
+    {
       return preffered;
     }
-    if (preffered.length === 0 && Math.max(...firstSeive) < Math.max(...pref)) {
-      preffered.push(Math.min(...firstSeive));
-    } else {
-      preffered.push(Math.max(...firstSeive));
+    if( preffered.length === 0 && Math.max( ... firstSeive ) < Math.max( ... pref ) )
+    {
+      preffered.push( Math.min( ... firstSeive ) );
+    }
+ else
+    {
+      preffered.push( Math.max( ... firstSeive ) );
     }
     return preffered;
   }
--- attempt.test.js
+++ attempt.test.js
@@ -1,74 +1,75 @@
 /* eslint-disable no-param-reassign */
-const _ = require('wTesting');
-const Attempts = require('./attempt');
+const _ = require( 'wTesting' );
+const Attempts = require( './attempt' );
 
 //
 
-function routine1(test) {
+function routine1( test )
+{
   test.case = 'First Test case';
-  const got = Attempts.attempts([240, 360, 720], [360, 720], [1080]);
-  const expected = [360];
-  test.identical(got, expected);
+  const got = Attempts.attempts( [ 240, 360, 720 ], [ 360, 720 ], [ 1080 ] );
+  const expected = [ 360 ];
+  test.identical( got, expected );
 
   test.case = 'Second Test case';
-  const got1 = Attempts.attempts([240, 720], [360, 720], [1080]);
-  const expected1 = [720];
-  test.identical(got1, expected1);
+  const got1 = Attempts.attempts( [ 240, 720 ], [ 360, 720 ], [ 1080 ] );
+  const expected1 = [ 720 ];
+  test.identical( got1, expected1 );
 
   test.case = 'Third Test case';
-  const got2 = Attempts.attempts([240], [360, 720], [1080]);
+  const got2 = Attempts.attempts( [ 240 ], [ 360, 720 ], [ 1080 ] );
   const expected2 = [];
-  test.identical(got2, expected2);
+  test.identical( got2, expected2 );
 
   test.case = 'Fourth Test case';
-  const got3 = Attempts.attempts([240, 360, 720], [240, 360, 720, 1080], [240, 360]);
-  const expected3 = [240, 360];
-  test.identical(got3, expected3);
+  const got3 = Attempts.attempts( [ 240, 360, 720 ], [ 240, 360, 720, 1080 ], [ 240, 360 ] );
+  const expected3 = [ 240, 360 ];
+  test.identical( got3, expected3 );
 
   test.case = 'Fifth Test case';
-  const got4 = Attempts.attempts([240, 720], [240, 360, 720, 1080], [240, 360]);
-  const expected4 = [240, 720];
-  test.identical(got4, expected4);
+  const got4 = Attempts.attempts( [ 240, 720 ], [ 240, 360, 720, 1080 ], [ 240, 360 ] );
+  const expected4 = [ 240, 720 ];
+  test.identical( got4, expected4 );
 
   test.case = 'Sixth Test case';
-  const got5 = Attempts.attempts([240, 720], [240, 360, 1080], [240]);
-  const expected5 = [240];
-  test.identical(got5, expected5);
+  const got5 = Attempts.attempts( [ 240, 720 ], [ 240, 360, 1080 ], [ 240 ] );
+  const expected5 = [ 240 ];
+  test.identical( got5, expected5 );
 
   test.case = 'Seventh Test case';
-  const got6 = Attempts.attempts([720], [240, 360, 1080], [240, 360]);
+  const got6 = Attempts.attempts( [ 720 ], [ 240, 360, 1080 ], [ 240, 360 ] );
   const expected6 = [];
-  test.identical(got6, expected6);
+  test.identical( got6, expected6 );
 
   test.case = 'Eighth Test case';
-  const got7 = Attempts.attempts([240, 360], [240, 360], [720, 1080]);
-  const expected7 = [360];
-  test.identical(got7, expected7);
+  const got7 = Attempts.attempts( [ 240, 360 ], [ 240, 360 ], [ 720, 1080 ] );
+  const expected7 = [ 360 ];
+  test.identical( got7, expected7 );
 
   test.case = 'Ninth Test case';
-  const got8 = Attempts.attempts([240, 360, 720], [360, 'any'], [360, 720]);
-  const expected8 = [360, 720];
-  test.identical(got8, expected8);
+  const got8 = Attempts.attempts( [ 240, 360, 720 ], [ 360, 'any' ], [ 360, 720 ] );
+  const expected8 = [ 360, 720 ];
+  test.identical( got8, expected8 );
 
   test.case = 'Tenth Test case';
-  const got9 = Attempts.attempts([240, 360, 720], [240, 360, 720], ['any', 720]);
-  const expected9 = [240, 360, 720];
-  test.identical(got9, expected9);
+  const got9 = Attempts.attempts( [ 240, 360, 720 ], [ 240, 360, 720 ], [ 'any', 720 ] );
+  const expected9 = [ 240, 360, 720 ];
+  test.identical( got9, expected9 );
 
   test.case = 'Eleventh Test case';
-  const got10 = Attempts.attempts([240, 360, 720], [360, 1080], ['any', 720]);
-  const expected10 = [360];
-  test.identical(got10, expected10);
+  const got10 = Attempts.attempts( [ 240, 360, 720 ], [ 360, 1080 ], [ 'any', 720 ] );
+  const expected10 = [ 360 ];
+  test.identical( got10, expected10 );
 
   test.case = 'Twelveth Test case';
-  const got11 = Attempts.attempts([240, 360, 720], [1080], ['any', 720]);
+  const got11 = Attempts.attempts( [ 240, 360, 720 ], [ 1080 ], [ 'any', 720 ] );
   const expected11 = [];
-  test.identical(got11, expected11);
+  test.identical( got11, expected11 );
 }
 
 let Self = {
-  name: 'Attempt',
-  tests: {
+  name : 'Attempt',
+  tests : {
     routine1,
 
   },
@@ -76,5 +77,5 @@ let Self = {
 
 //
 
-Self = wTestSuite(Self);
-if (typeof module !== 'undefined' && !module.parent) wTester.test(Self.name);
+Self = wTestSuite( Self );
+if( typeof module !== 'undefined' && !module.parent ) wTester.test( Self.name );
